524f87f69a71593061b332c89b25c88ae72a8c37
Anwar <anwarhussaini160@gmail.com>
2023-08-25 03:57:31 +0430

    Resolve the linters error

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,4 +1,3 @@
-# This module provides implementations of several common Enumerable methods, which are commonly used for iterating and working with collections in Ruby.
 module MyEnumerable
   # This method iterates over each item in the collection and yields it to the block.
   def all?
