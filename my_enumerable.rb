e991071dbf925fefc722e84a56ab9d8950041836
Anwar <anwarhussaini160@gmail.com>
2023-08-25 03:49:43 +0430

    Add all function

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,8 @@
 # This module provides implementations of several common Enumerable methods, which are commonly used for iterating and working with collections in Ruby.
 module MyEnumerable
+  # This method iterates over each item in the collection and yields it to the block.
+  def all?
+    each { |item| return false unless yield(item) }
+    true
+  end
 end
